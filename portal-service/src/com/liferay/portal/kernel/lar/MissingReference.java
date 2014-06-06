1a92284e1c61f921c99dd93b6f1f9ae547d5a9a5
Máté Thurzó <mate.thurzo@liferay.com>
2014-06-06 16:18:04 +0200

    LPS-44283 groupId coming from the missing reference XML element might not make sense on the live environment, we should look up the group and only display the message if it's possible

--- portal-service/src/com/liferay/portal/kernel/lar/MissingReference.java
+++ portal-service/src/com/liferay/portal/kernel/lar/MissingReference.java
@@ -33,7 +33,6 @@ public class MissingReference implements Serializable {
 		_classPK = element.attributeValue("class-pk");
 		_displayName = GetterUtil.getString(
 			element.attributeValue("display-name"));
-		_groupId = element.attributeValue("group-id");
 		_referrerClassName = element.attributeValue("referrer-class-name");
 		_type = GetterUtil.getString(element.attributeValue("type"));
 
@@ -65,7 +64,7 @@ public class MissingReference implements Serializable {
 		return _displayName;
 	}
 
-	public String getGroupId() {
+	public long getGroupId() {
 		return _groupId;
 	}
 
@@ -85,10 +84,14 @@ public class MissingReference implements Serializable {
 		return _type;
 	}
 
+	public void setGroupId(long groupId) {
+		_groupId = groupId;
+	}
+
 	private String _className;
 	private String _classPK;
 	private String _displayName;
-	private String _groupId;
+	private long _groupId;
 	private String _referrerClassName;
 	private Map<String, String> _referrers = new HashMap<String, String>();
 	private String _type;
