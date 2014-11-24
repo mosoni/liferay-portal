81de9d0d5fbc1d59a96e0e4fea45589c2dc8625a
Michael Hashimoto <michael.hashimoto@liferay.com>
2014-11-24 10:59:08 -0800

    LPS-51331 SF

--- portal-impl/src/com/liferay/portal/tools/seleniumbuilder/SeleniumBuilderFileUtil.java
+++ portal-impl/src/com/liferay/portal/tools/seleniumbuilder/SeleniumBuilderFileUtil.java
@@ -977,8 +977,6 @@ public class SeleniumBuilderFileUtil {
 				if (!attributeName.equals("function") &&
 					!attributeName.equals("ignore-javascript-error") &&
 					!attributeName.equals("line-number") &&
-					!attributeName.equals("locator1") &&
-					!attributeName.equals("locator2") &&
 					!attributeName.startsWith("locator") &&
 					!attributeName.startsWith("value")) {
 
