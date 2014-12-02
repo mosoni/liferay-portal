0019000cf6c8bf438c59026bac4e4d3bafe426b3
Pablo Pinheiro <pablo.carvalho@liferay.com>
2014-12-02 16:01:42 -0300

    LPS-50592 Changed date inputs ids to match their names.

--- portal-impl/src/com/liferay/portal/util/HtmlImpl.java
+++ portal-impl/src/com/liferay/portal/util/HtmlImpl.java
@@ -355,8 +355,9 @@ public class HtmlImpl implements Html {
 
 			if (c != CharPool.UNDERLINE) {
 				sb.append(StringUtil.toHexString(c));
-				sb.append(CharPool.UNDERLINE);
 			}
+
+			sb.append(CharPool.UNDERLINE);
 
 			lastReplacementIndex = i + 1;
 		}
