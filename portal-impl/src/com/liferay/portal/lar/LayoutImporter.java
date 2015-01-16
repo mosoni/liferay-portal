liferay/liferay-central-test-1

LPS-46469 plid is what you really want

portal-impl/src/com/liferay/portal/lar/LayoutImporter.java
@@ -543,9 +543,9 @@ public class LayoutImporter {
 				long layoutId = GetterUtil.getLong(
 					portletElement.attributeValue("layout-id"));
 
-				Layout layout = layouts.get(layoutId);
-
 				long plid = LayoutConstants.DEFAULT_PLID;
+
+				Layout layout = layouts.get(layoutId);
 
 				if (layout != null) {
 					plid = layout.getPlid();

mosoni/liferay-portal

portal-impl/src/com/liferay/portal/lar/LayoutImporter.java
@@ -701,9 +701,9 @@ public class LayoutImporter {
 				long layoutId = GetterUtil.getLong(
 					portletElement.attributeValue("layout-id"));
 
-				Layout layout = newLayoutsMap.get(layoutId);
-
 				long plid = LayoutConstants.DEFAULT_PLID;
+
+				Layout layout = newLayoutsMap.get(layoutId);
 
 				if (layout != null) {
 					plid = layout.getPlid();